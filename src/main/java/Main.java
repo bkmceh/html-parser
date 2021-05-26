54ae6fc8fb68258b95ceaf3f6c6165e2adbe956a
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2021-05-26 23:47:29 +0300

    change Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,50 @@
-import java.io.BufferedReader;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.HashSet;
+
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Test");
+
+    public static void main(String[] args) throws IOException {
+        DelimiterList delimiterList =
+                new DelimiterList(new Character[]{
+                        ' ', ',', '.', '!', '?', '"', ';', ':', '[', ']', '(', ')'});//TODO for example \n or \t
+
+        String html = Jsoup.connect(InputURL.url).get().html();
+        Document htmlDoc = Jsoup.parse(html);
+        String words = htmlDoc.body().text();
+
+        findWords(words, delimiterList.delimiters);
+        printResult();
+    }
+
+    private static void findWords(String words, HashSet<Character> delimeters) {
+
+        char[] chars = words.toCharArray();
+
+        int indexBegin = 0;
+
+        for (int i = 0; i < chars.length; i++) {
+            if (delimeters.contains(chars[i])) {
+                String word = words.substring(indexBegin, i);
+                indexBegin = i + 1;
+                if (word.length() == 0) continue;
+                if (!WordDictionary.wordDictionary.containsKey(word)) {
+                    WordDictionary.wordDictionary.put(word, 1);
+                } else {
+                    WordDictionary.wordDictionary.put(word, WordDictionary.wordDictionary.get(word) + 1);
+                }
+            }
+        }
+
+    }
+
+    private static void printResult() {
+        for (String key :
+                WordDictionary.wordDictionary.keySet()) {
+            System.out.printf("%s: %s%n", key, WordDictionary.wordDictionary.get(key));
+        }
     }
 }
+
