36a98aa8cca4118b8815524698112dbfdfe72296
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2021-05-28 01:56:37 +0300

    store text of body of html

---
+++ src/main/java/HtmlPageBody.java
@@ -0,0 +1,12 @@
+public class HtmlPageBody {
+
+    private final String text;
+
+    public HtmlPageBody(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
