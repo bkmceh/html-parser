c0e1992ddc3222188f4450d85b4a8246cac87c45
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2021-05-28 01:53:27 +0300

    change Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,50 +1,16 @@
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import java.io.IOException;
-import java.util.HashSet;
-
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        DelimiterList delimiterList =
-                new DelimiterList(new Character[]{
-                        ' ', ',', '.', '!', '?', '"', ';', ':', '[', ']', '(', ')'});//TODO for example \n or \t
+    private final static String URL = "https://www.simbirsoft.com/";
 
-        String html = Jsoup.connect(InputURL.url).get().html();
-        Document htmlDoc = Jsoup.parse(html);
-        String words = htmlDoc.body().text();
+    private final static char[] DELIMITERS = {' ', ',', '.', '!', '?', '"', ';', ':', '[', ']', '(', ')', '\n', '\r', '\t'};
 
-        findWords(words, delimiterList.delimiters);
-        printResult();
-    }
+    public static void main(String[] args){
 
-    private static void findWords(String words, HashSet<Character> delimeters) {
+        final HtmlPageBody body = new HtmlDownloader(URL).download();
+        final HtmlPageWordStats stat = new WordStats(body).count(DELIMITERS);
 
-        char[] chars = words.toCharArray();
+        stat.printResult();
 
-        int indexBegin = 0;
-
-        for (int i = 0; i < chars.length; i++) {
-            if (delimeters.contains(chars[i])) {
-                String word = words.substring(indexBegin, i);
-                indexBegin = i + 1;
-                if (word.length() == 0) continue;
-                if (!WordDictionary.wordDictionary.containsKey(word)) {
-                    WordDictionary.wordDictionary.put(word, 1);
-                } else {
-                    WordDictionary.wordDictionary.put(word, WordDictionary.wordDictionary.get(word) + 1);
-                }
-            }
-        }
-
-    }
-
-    private static void printResult() {
-        for (String key :
-                WordDictionary.wordDictionary.keySet()) {
-            System.out.printf("%s: %s%n", key, WordDictionary.wordDictionary.get(key));
-        }
     }
 }
 
