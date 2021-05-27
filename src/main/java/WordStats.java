875198a23ebe5d3e0eba4f48a1df25e4cf76db7a
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2021-05-28 01:54:54 +0300

    count words

---
+++ src/main/java/WordStats.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+
+public class WordStats {
+
+    private final HtmlPageBody body;
+
+    private final HashMap<String, Integer> words = new HashMap<>();
+
+    public WordStats(HtmlPageBody body) {
+        this.body = body;
+    }
+
+    public HtmlPageWordStats count(char[] delimiters) {
+
+        String text = body.getText();
+
+        char[] chars = text.toCharArray();
+
+        int indexBegin = 0;
+
+        for (int i = 0; i < chars.length; i++) {
+            for (char delimiter :
+                    delimiters) {
+                if (delimiter == chars[i]) {
+                    String word = text.substring(indexBegin, i).toUpperCase();
+                    indexBegin = i + 1;
+                    if (word.length() == 0) continue;
+
+                    if (!words.containsKey(word)) {
+                        words.put(word, 1);
+                    } else {
+                        words.put(word, words.get(word) + 1);
+                    }
+                }
+            }
+        }
+        return new HtmlPageWordStats(words);
+    }
+}
