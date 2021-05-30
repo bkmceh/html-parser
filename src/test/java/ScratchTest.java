5dad6ecf13703d3da6933aac835648ba2a92ae7d
bkmceh <IfhnlbyjdBkmceh@yandex.ru>
2021-05-30 15:34:53 +0300

    changed tests

--- src/test/java/ScratchTest.java
+++ src/test/java/ScratchTest.java
@@ -1,43 +1,37 @@
 import org.junit.Test;
 
+import static org.junit.Assert.*;
 
-public class HtmlDownloaderTest {
-
-    HtmlDownloader htmlDownloader;
+public class ScratchTest {
 
     @Test
     public void downloadWhenURLisFull() {
         String URL = "https://www.simbirsoft.com/";
-        htmlDownloader = new HtmlDownloader(URL);
-        htmlDownloader.download();
+        new Scratch(URL).download();
     }
 
     @Test
     public void downloadWhenURLisFull2() {
         String URL = "https://www.simbirsoft.com";
-        htmlDownloader = new HtmlDownloader(URL);
-        htmlDownloader.download();
+        new Scratch(URL).download();
     }
 
     @Test(expected = RuntimeException.class)
     public void downloadWhenURLisValidButNotFull() {
         String URL = "www.simbirsoft.com/";
-        htmlDownloader = new HtmlDownloader(URL);
-        htmlDownloader.download();
+        new Scratch(URL).download();
     }
 
     @Test(expected = RuntimeException.class)
     public void downloadWhenURLisNotValid() {
         String URL = "hts://ww.simbirft.co";
-        htmlDownloader = new HtmlDownloader(URL);
-        htmlDownloader.download();
+        new Scratch(URL).download();
     }
 
     @Test(expected = RuntimeException.class)
     public void downloadWhenURLisValidButCannotDownload() {
         String URL = "https://ranobelib.me/mushoku-tensei/v29/c6";
-        htmlDownloader = new HtmlDownloader(URL);
-        htmlDownloader.download();
+        new Scratch(URL).download();
     }
 
 
